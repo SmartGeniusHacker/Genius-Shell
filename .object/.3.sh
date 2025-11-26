84d6069cee0d7a566612b828ba46e7f1277dbb74
smartgeniushacker <smartgeniushacker@gmail.com>
2025-11-26 23:47:27 +0530

    updated all files

--- .object/.3.sh
+++ .object/.3.sh
@@ -1,24 +1,94 @@
 #!/usr/bin/bash
+
 git clone https://github.com/zsh-users/zsh-autosuggestions /data/data/com.termux/files/home/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /data/data/com.termux/files/home/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
 clear
-PUT(){ echo -en "\033[${1};${2}H";}
-clear;toilet -t -f mono12  "Genius" --gay -F border
+
+# Function to center text
+center_text() {
+    local term_width=$(tput cols)
+    local text="$1"
+    local text_width=${#text}
+    local padding=$(( (term_width - text_width) / 2 ))
+    printf "%*s%s\n" $padding "" "$text"
+}
+
+# GENIUS Title (Centered automatically)
+toilet -t -f mono12 "Genius" --gay -F border
+
 echo ""
-PUT 11 40
-echo -e "\e[92mBy\e[1;93m Shivam Kumar\e[1;95m /\e[1;96m Genius-Shell"
-PUT 12 3
+
+# Center your credit line
+center_text $'\e[92mBy\e[1;93m Shivam Kumar\e[1;95m /\e[1;96m Genius-Shell\e[0m'
+
 echo ""
 echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91mTool Name \e[1;36mGenius-Shell \e[1;91m by \e[1;36mShivam Kumar\e[0m"
 echo
 echo -e "  \e[101;1;39mNOTE\e[0;1;33m Use upto 9 words\e[0m"
 echo ""
+
 cd ~/Genius-Shell/.object
 echo -e '\e[1;96m'
 read -p '  Type Banner Name ❯ ' name
 sed -e "s/\PROC/$name/g" .2zshrc > $HOME/.zshrc
-echo  ""
+
+echo ""
 echo -e '\e[1;96m'
 read -p '  Type Shell Name ❯ ' name
 sed -e "s/\Genius/$name/g" .genius-shell.zsh-theme > $HOME/.oh-my-zsh/themes/genius-shell.zsh-theme
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #!/usr/bin/bash
+# git clone https://github.com/zsh-users/zsh-autosuggestions /data/data/com.termux/files/home/.oh-my-zsh/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /data/data/com.termux/files/home/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+# clear
+# PUT(){ echo -en "\033[${1};${2}H";}
+# clear;toilet -t -f mono12  "Genius" --gay -F border
+# echo ""
+# PUT 11 40
+# echo -e "\e[92mBy\e[1;93m Shivam Kumar\e[1;95m /\e[1;96m Genius-Shell"
+# PUT 12 3
+# echo ""
+# echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91mTool Name \e[1;36mGenius-Shell \e[1;91m by \e[1;36mShivam Kumar\e[0m"
+# echo
+# echo -e "  \e[101;1;39mNOTE\e[0;1;33m Use upto 9 words\e[0m"
+# echo ""
+# cd ~/Genius-Shell/.object
+# echo -e '\e[1;96m'
+# read -p '  Type Banner Name ❯ ' name
+# sed -e "s/\PROC/$name/g" .2zshrc > $HOME/.zshrc
+# echo  ""
+# echo -e '\e[1;96m'
+# read -p '  Type Shell Name ❯ ' name
+# sed -e "s/\Genius/$name/g" .genius-shell.zsh-theme > $HOME/.oh-my-zsh/themes/genius-shell.zsh-theme
